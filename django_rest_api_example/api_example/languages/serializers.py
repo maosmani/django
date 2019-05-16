eb72b8ade1a8315a8b04a74e8aba6d287ca649ea
maosmani <33595506+maosmani@users.noreply.github.com>
2019-05-17 00:40:26 +0100

    Add files via upload

---
+++ django_rest_api_example/api_example/languages/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Language
+class LanguageSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Language
+         fields = ('id','name','paradigm')
